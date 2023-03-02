36fbddfd597013b930026c8f0494a0c23e1f2ed7
Mor Gazith <mor.gazith@benchmarkemail.com>
2023-03-02 15:52:43 +0200

    CON-1564 / Remove ahead-of-time flag from all in-use FullContact libraries

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject fullcontact/full.json "0.10.2"
+(defproject fullcontact/full.json "0.10.3"
   :description "Read and write JSON (Cheshire extension)."
-  :url "https://github.com/contactsplus/full.json"
+  :url "https://github.com/contactsplusapp/full.json"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
@@ -9,10 +9,10 @@
                  ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local"}]]
   :deploy-repositories [["releases" {:url "https://contactsplus.jfrog.io/artifactory/libs-release-local" :sign-releases false}]
                         ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local" :sign-releases false}]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [cheshire "5.6.0"]
                  [fullcontact/camelsnake "0.9.0"]
-                 [fullcontact/full.core "0.10.0"]]
+                 [fullcontact/full.core "1.1.3"]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
@@ -21,6 +21,5 @@
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
-  :aot :all
   :plugins [[lein-midje "3.1.3"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.10.9"]]}})
